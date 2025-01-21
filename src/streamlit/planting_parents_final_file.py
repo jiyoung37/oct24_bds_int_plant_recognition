ecff1a538d74f6d62b3f2565a48a37aae145ddae
Nielsvd06 <n.a.vanduijn@gmail.com>
2025-01-21 12:00:31 +0100

    Latest updated version 12:00 21-Jan-2025
    Merge between Yannik and Niels' version.

--- src/streamlit/planting_parents_final_file.py
+++ src/streamlit/planting_parents_final_file.py
@@ -2,18 +2,14 @@ import streamlit as st
 import os
 import pandas as pd
 
-# # Check the working directory and all files which are contained in it
-# st.write("Current working directory:", os.getcwd())
-# st.write("Files in current directory:", os.listdir())
-
 # Load the CSV file into a DataFrame
-df = pd.read_csv("src/streamlit/plant_dataset.csv")
+df = pd.read_csv("./plant_dataset.csv")
 
 # start with the streamlit app
 
 # sidebar and page navigation
 st.sidebar.title("Table of contents")
-pages = ["Home", "Data overview", "Data analysis & visualization", "Model training", "Model Interpretability", "Conclusion", "Predict your plant"]
+pages = ["Home", "Data overview", "Data exploration", "Model training", "Model Interpretability", "Conclusion", "Predict your plant"]
 page = st.sidebar.radio("Go to", pages)
 
 # set a dynamic title for each page
@@ -22,17 +18,74 @@ page = st.sidebar.radio("Go to", pages)
 if page == pages[0]:
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        st.image("src/visualization/Planting_parents_logo.png", use_container_width=True)
-    st.header("Introduction")
-    st.write("Welcome to the site of the Planting Parents, where we have trained an AI model to recognise different species of plants and whether they are sick or healthy."
-    " We sincerely hope you enjoy our page, that you may find it informative and recognise the plants you want to recognise")
-    st.write("### 2nd paragraph")
+        st.image("./../visualization/Planting_parents_logo.png", use_column_width = True)
+        st.header("Introduction")
+        st.write("Welcome to the site of the Planting Parents, where we have trained an AI model to recognise 14 different species of plants and whether they are sick or healthy by analysing 20 plant diseases."
+             " We're a group of young parents who value life and share an interest in growing plant life as well. It has been a great and rewarding challenge to present to you this app with our findings and results." 
+             " We sincerely hope you enjoy our page and that you may find it informative and recognise the plants you want to recognise. ")
+        st.write("**The planting parents,**")
+        st.write("**Lara, Ji-Young, Yannik & Niels**")
+
+
+####################
+# DATA OVERVIEW    #
+####################
+
 
 elif page == pages[1]:
-    st.write("### Scope of the data")
-    st.write("\n", "Data Frame shape:", "\n", df.shape, "\n")
-    st.dataframe(df.describe())
-    st.dataframe(df.head(6))
+    st.write("In the data overview we'll go a bit deeper in the data we've explored and the data we ultimately used after which we defined our pre-processing steps ready to be analysed for the exploration phase of the project." 
+              " Click through the tabs below to learn more: ")
+    
+    tab1, tab2, tab3 = st.tabs(["The numbers", "plants & diseases", "Pre-processing steps"])
+    
+    with tab1: # The numbers
+        st.write("For our initial research into data, we stumbled upon many datasets that had some sort of plant recognition dataset of many different species from the website Kaggle. ")
+        
+        st.write("""
+            We work with a database called ‘New Plant Diseases Dataset’ which is a public repository available on Kaggle.
+
+It includes multiple subfolders; Train, Valid and Test. These subfolders contain subfolders per plant where the target image files (jpg) are stored, totalling 87,900 images.
+
+    Files in train_path:            70,295
+    Files in valid_path:            17,572
+    Files in test_path:                 33
+    Total files:                    87,900
+                 """)
+        st.write("\n", "Data Frame shape:", "\n", df.shape, "\n")
+        st.write("All images have 256 * 256 as their image size.")
+        st.write("In the below table overview you'll find all the names of the plant species and plant diseases: ")
+        st.image("./../visualization/Plants_&_Diseases_count.png", use_column_width = True)
+    
+    with tab2: # Plants & diseases
+        st.write("")
+        st.image("./../visualization/diseased_plant_samples.png")
+
+        st.write("")
+        st.image("./../visualization/healthy_plant_samples.png")
+
+    with tab3: # Pre-processing steps
+        st.write(""" As for the preprocessing phase we followed these steps to get to the analysis and visualisation phase of the files:
+                 
+                1.1 Import kaggle hub and download dataset. 
+	            1.2 Define the paths.
+	            1.3 Importing necessary libraries for data processing and visualization.
+                1.4 Creating a DataFrame with Train and Valid.
+                1.5 Check for missing values and duplicates between train and valid subset.
+
+        The data was already well sorted, indexed and labelled. We only had to create an import path and created lists to work of from: 
+        # Initialize lists
+        image_paths = []
+        species_labels = []
+        disease_labels = []
+        dataset_split = []
+
+        We looked for missing values and duplicates even crosschecked between the train and valid folders and found none. """)
+
+
+####################
+# DATA EXPLORATION #
+####################
+
 
 elif page == pages[2]:
     st.write("### 1st paragraph")

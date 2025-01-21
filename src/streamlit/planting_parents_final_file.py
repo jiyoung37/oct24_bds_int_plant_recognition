c3a2834c9bed0776453286dc568720576819f615
schytze0 <schytze@gmx.de>
2025-01-21 23:20:46 +0100

    Changed again to use_container_width

--- src/streamlit/planting_parents_final_file.py
+++ src/streamlit/planting_parents_final_file.py
@@ -38,13 +38,14 @@ page = st.sidebar.radio("Go to", pages)
 if page == pages[0]:
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        st.image("src/visualization/Planting_parents_logo.png", use_column_width=True)
+        st.image("src/visualization/Planting_parents_logo.png", use_container_width=True)
         st.header("Introduction")
-        st.write("Welcome to the site of the Planting Parents, where we have trained an AI model to recognise 14 different species of plants and whether they are sick or healthy by analysing 20 plant diseases."
-             " We're a group of young parents who value life and share an interest in growing plant life as well. It has been a great and rewarding challenge to present to you this app with our findings and results." 
-             " We sincerely hope you enjoy our page and that you may find it informative and recognise the plants you want to recognise. ")
-        st.write("**The planting parents,**")
-        st.write("**Lara, Ji-Young, Yannik & Niels**")
+
+    st.write("Welcome to the site of the Planting Parents, where we have trained an AI model to recognise 14 different species of plants and whether they are sick or healthy by analysing 20 plant diseases."
+            " We're a group of young parents who value life and share an interest in growing plant life as well. It has been a great and rewarding challenge to present to you this app with our findings and results." 
+            " We sincerely hope you enjoy our page and that you may find it informative and recognise the plants you want to recognise. ")
+    st.write("**The planting parents,**")
+    st.write("**Lara, Ji-Young, Yannik & Niels**")
 
 
 ####################
@@ -74,7 +75,7 @@ It includes multiple subfolders; Train, Valid and Test. These subfolders contain
         st.write("\n", "Data Frame shape:", "\n", df.shape, "\n")
         st.write("All images have 256 * 256 as their image size.")
         st.write("In the below table overview you'll find all the names of the plant species and plant diseases: ")
-        st.image("src/visualization/Plants_&_Diseases_count.png", use_column_width=True)
+        st.image("src/visualization/Plants_&_Diseases_count.png", use_container_width=True)
     
     with tab2: # Plants & diseases
         st.write("")

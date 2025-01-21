74ad51d82e3fbf71aa484f6ef9465a21da3cdac9
schytze0 <schytze@gmx.de>
2025-01-21 11:12:53 +0100

    Modified all file paths

--- src/streamlit/planting_parents_final_file.py
+++ src/streamlit/planting_parents_final_file.py
@@ -2,12 +2,12 @@ import streamlit as st
 import os
 import pandas as pd
 
-# Check the working directory and all files which are contained in it
-st.write("Current working directory:", os.getcwd())
-st.write("Files in current directory:", os.listdir())
+# # Check the working directory and all files which are contained in it
+# st.write("Current working directory:", os.getcwd())
+# st.write("Files in current directory:", os.listdir())
 
 # Load the CSV file into a DataFrame
-# df = pd.read_csv("./plant_dataset.csv")
+df = pd.read_csv("src/streamlit/plant_dataset.csv")
 
 # start with the streamlit app
 
@@ -21,8 +21,8 @@ page = st.sidebar.radio("Go to", pages)
 
 if page == pages[0]:
     col1, col2, col3 = st.columns([1, 2, 1])
-    # with col2:
-    #     st.image("./../visualization/Planting_parents_logo.png", use_container_width=True)
+    with col2:
+        st.image("src/visualization/Planting_parents_logo.png", use_container_width=True)
     st.header("Introduction")
     st.write("Welcome to the site of the Planting Parents, where we have trained an AI model to recognise different species of plants and whether they are sick or healthy."
     " We sincerely hope you enjoy our page, that you may find it informative and recognise the plants you want to recognise")
@@ -30,9 +30,9 @@ if page == pages[0]:
 
 elif page == pages[1]:
     st.write("### Scope of the data")
-    # st.write("\n", "Data Frame shape:", "\n", df.shape, "\n")
-    # st.dataframe(df.describe())
-    # st.dataframe(df.head(6))
+    st.write("\n", "Data Frame shape:", "\n", df.shape, "\n")
+    st.dataframe(df.describe())
+    st.dataframe(df.head(6))
 
 elif page == pages[2]:
     st.write("### 1st paragraph")
